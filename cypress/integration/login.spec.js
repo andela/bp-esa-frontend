34b16acee2bfc01af156a33df2307bbe8a67992a
koechkevin <koechkevin92@gmail.com>
2019-04-23 18:42:21 +0300

    feat(end2end): login tests
     - add end to end tests for login
     - add environment variables example for end to end tests
    
    [Delivers #165530727]

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,40 @@
+Cypress.on('window:before:load', (win) => {
+  win.open = cy.stub().as('windowOpen')
+});
+const state = Cypress.env('state');
+
+describe('Login Page', () => {
+  beforeEach(() => {
+    cy
+      .visit('/')
+  });
+
+  it('loads Andela Logo',() => {
+    cy
+      .get('.logo')
+      .should('be.visible');
+  });
+
+  it('should render button for sign in', () => {
+    cy
+      .get('button')
+      .should('have.class', 'sign-in-button-container')
+      .should('be.visible')
+  });
+
+  it('opens a window to enable login with google when sign in button is clicked', () => {
+    cy
+      .get('button')
+      .click({multiple: true})
+      .get('@windowOpen')
+      .should('be.called');
+  });
+
+  it('lands to home page if login is successful', () =>{
+    window.localStorage.setItem('state', JSON.stringify(state));
+    cy
+      .visit('/')
+      .get('.stats-wrapper')
+      .should('be.visible')
+  });
+});
