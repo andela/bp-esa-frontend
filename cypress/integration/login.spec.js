49c73fa6f396702825d6ea1cc1da11033153256d
michael nthiwa <michaelnthiwa@michaels-MacBook-Pro-2.local>
2019-05-07 10:05:38 +0300

    ch(end2end test in circleCI) run end to end in circleCI  - ensure that end2end test run in circleCI
    [Deliver 165827850]

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,15 +1,14 @@
 Cypress.on('window:before:load', (win) => {
-  win.open = cy.stub().as('windowOpen')
+  win.open = cy.stub().as('windowOpen');
 });
 const state = Cypress.env('state');
 
 describe('Login Page', () => {
   beforeEach(() => {
-    cy
-      .visit('/')
+    cy.visit('/');
   });
 
-  it('loads Andela Logo',() => {
+  it('loads Andela Logo', () => {
     cy
       .get('.logo')
       .should('be.visible');
@@ -19,22 +18,28 @@ describe('Login Page', () => {
     cy
       .get('button')
       .should('have.class', 'sign-in-button-container')
-      .should('be.visible')
+      .should('be.visible');
   });
 
   it('opens a window to enable login with google when sign in button is clicked', () => {
     cy
       .get('button')
-      .click({multiple: true})
+      .click({ multiple: true })
       .get('@windowOpen')
       .should('be.called');
   });
 
-  it('lands to home page if login is successful', () =>{
+  it('lands to home page if login is successful', () => {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:6050',
+      response: [],
+    });
     window.localStorage.setItem('state', JSON.stringify(state));
     cy
       .visit('/')
       .get('.stats-wrapper')
-      .should('be.visible')
+      .should('be.visible');
   });
 });
